5d9804c549fee933b78eb5bd05690b3b4795ea10
AngelGiurov <angelgiurov2000@abv.bg>
2024-03-24 22:48:55 +0200

    initial commit

---
+++ src/main/java/com/fitnesstrackingapplication/UserFitnessTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesstrackingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFitnessTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFitnessTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnesstrackingapplication/UserFitnessTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesstrackingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFitnessTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
