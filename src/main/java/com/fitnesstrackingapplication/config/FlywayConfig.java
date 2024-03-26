975e973a16573b96de8f772852aebf7306047578
AngelGiurov <angelgiurov2000@abv.bg>
2024-03-26 12:17:35 +0200

    FlywayConfig implementation

---
+++ src/main/java/com/fitnesstrackingapplication/config/FlywayConfig.java
@@ -0,0 +1,55 @@
+package com.fitnesstrackingapplication.config;
+
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+
+@Configuration("flyway_config")
+@RequiredArgsConstructor
+public class FlywayConfig {
+
+    private static final String DEFAULT_SCHEMA_LOCATION = "db/migration";
+
+
+    @Value("${DATABASE}")
+    private String database;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+
+    public DataSource buildDataSource() {
+        return DataSourceBuilder.create()
+                .url(url)
+                .driverClassName(driverClassName)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+    @PostConstruct
+    void initTenantMigration() {
+        Flyway.configure()
+                .dataSource(buildDataSource())
+                .locations(DEFAULT_SCHEMA_LOCATION)
+                .schemas("user_fitness_tracking_application")
+                .load()
+                .migrate();
+    }
+
+}
